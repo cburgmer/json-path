01960bc114aa2ac2f28736b47ce1cac9cc12edca
Christoph Burgmer <christoph.burgmer@gmail.com>
2016-12-07 22:32:22 +0800

    Include lein-midje as plugin so we can run 'line midje' without having to include it from ~/.lein/profiles.clj

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "JSON Path for Clojure data structures"
   :url "http://github.com/gga/json-path"
   :dependencies [[org.clojure/clojure "1.5.0"]]
-  :profiles {:dev {:dependencies [[midje "1.5.0"]
-                                  [lein-midje "3.0.0"]]}})
+  :profiles {:dev {:plugins [[lein-midje "3.2.1"]]
+                   :dependencies [[midje "1.6.0"]]}})
