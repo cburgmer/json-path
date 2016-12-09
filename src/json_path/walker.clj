526617e80b4f4190565f7b3aa564cff09b4bfbdf
Christoph Burgmer <christoph.burgmer@gmail.com>
2016-12-09 20:41:37 +0800

    WIP

--- src/json_path/walker.clj
+++ src/json_path/walker.clj
@@ -59,3 +59,59 @@
     (if continuation
       (walk continuation (assoc context :current down-obj))
       down-obj)))
+
+
+(defn eval-eq-expr-ng [op-form context operands]
+  (apply op-form (map #(eval-expr-ng % context) operands)))
+
+(defn eval-expr-ng [[expr-type & operands :as expr] context]
+  (let [ops {:eq =, :neq not=, :lt <, :lt-eq <=, :gt >, :gt-eq >=}]
+    (cond
+      (contains? ops expr-type) (eval-eq-expr-ng (expr-type ops) context operands)
+      (= expr-type :val) (first operands)
+      (= expr-type :path) (first (walk-ng expr context)))))
+
+(defn select-by-ng [[opcode & operands :as obj-spec] context]
+  (cond
+    (sequential? (:current context)) (vec (flatten (map first (filter #(not (empty? %))
+                                                           (map #(select-by-ng obj-spec (assoc context :current %))
+                                                                (:current context))))))
+    :else (cond
+            (= (first operands) "*") (vec (vals (:current context)))
+            :else (let [key (keyword (first operands))]
+                    [(key (:current context)) [key]]))))
+
+(defn walk-path-ng [[next & parts] context]
+  (cond
+    (nil? next) [(:current context) []]
+    (= [:root] next) (walk-path-ng parts (assoc context :current (:root context)))
+    (= [:child] next) (walk-path-ng parts context)
+    (= [:current] next) (walk-path-ng parts context)
+    (= [:all-children] next) (walk-path parts (assoc context :current (vec (concat [(:current context)]
+                                                                                   (obj-aggregator (:current context))))))
+    (= :key (first next)) (let [[value key] (select-by-ng next context)
+                                [result downstream-key] (walk-path-ng parts (assoc context :current value))]
+                            [result (cons key downstream-key)])))
+
+(defn walk-selector-ng [sel-expr context]
+  (cond
+    (= :index (first sel-expr)) (if (sequential? (:current context))
+                                  (let [sel (nth sel-expr 1)]
+                                    (if (= "*" sel)
+                                      (let [current (:current context)]
+                                        [current (range (count current))])
+                                      (let [index (Integer/parseInt sel)]
+                                        [(nth (:current context) index) [index]])))
+                                  (throw (Exception. "object must be an array.")))
+    (= :filter (first sel-expr)) (->> (:current context)
+                                      (keep-indexed (fn [i e] (if (eval-expr-ng (nth sel-expr 1) (assoc context :current e)) [e i])))
+                                      (apply map vector))))
+
+(defn walk-ng [[opcode operand continuation] context]
+  (let [[down-obj down-key] (cond
+                   (= opcode :path) (walk-path-ng operand context)
+                   (= opcode :selector) (walk-selector-ng operand context))]
+    (if continuation
+      (let [[obj key] (walk-ng continuation (assoc context :current down-obj))]
+        [obj (vec (concat down-key key))])
+      [down-obj down-key])))
--- test/json_path/test/walker_test.clj
+++ test/json_path/test/walker_test.clj
@@ -5,45 +5,45 @@
 (unfinished)
 
 (facts
-  (eval-expr [:eq [:val "a"] [:val "b"]] {}) => falsey
-  (eval-expr [:eq [:val "a"] [:val "a"]] {}) => truthy
-  (eval-expr [:neq [:val "a"] [:val "b"]] {}) => truthy
-  (eval-expr [:lt [:val 10] [:val 11]] {}) => truthy
-  (eval-expr [:lt-eq [:val 10] [:val 10]] {}) => truthy
-  (eval-expr [:gt [:val 10] [:val 9]] {}) => truthy
-  (eval-expr [:gt-eq [:val 10] [:val 10]] {}) => truthy
-  (eval-expr [:path [[:key "foo"]]] {:current {:foo "bar"}}) => "bar"
-  (eval-expr [:eq [:path [[:key "foo"]]] [:val "bar"]] {:current {:foo "bar"}}) => truthy)
+  (eval-expr-ng [:eq [:val "a"] [:val "b"]] {}) => falsey
+  (eval-expr-ng [:eq [:val "a"] [:val "a"]] {}) => truthy
+  (eval-expr-ng [:neq [:val "a"] [:val "b"]] {}) => truthy
+  (eval-expr-ng [:lt [:val 10] [:val 11]] {}) => truthy
+  (eval-expr-ng [:lt-eq [:val 10] [:val 10]] {}) => truthy
+  (eval-expr-ng [:gt [:val 10] [:val 9]] {}) => truthy
+  (eval-expr-ng [:gt-eq [:val 10] [:val 10]] {}) => truthy
+  (eval-expr-ng [:path [[:key "foo"]]] {:current {:foo "bar"}}) => "bar"
+  (eval-expr-ng [:eq [:path [[:key "foo"]]] [:val "bar"]] {:current {:foo "bar"}}) => truthy)
 
 (facts
-  (select-by [:key "hello"] {:current {:hello "world"}}) => "world"
-  (select-by [:key "hello"] {:current [{:hello "foo"} {:hello "bar"}]}) => ["foo" "bar"]
+  (select-by-ng [:key "hello"] {:current {:hello "world"}}) => ["world" [:hello]]
+  (select-by-ng [:key "hello"] {:current [{:hello "foo"} {:hello "bar"}]}) => [["foo" "bar"] [[0 :hello] [1 :hello]]]
   (select-by [:key "hello"] {:current [{:blah "foo"} {:hello "bar"}]}) => [ "bar"]
   (select-by [:key "*"] {:current {:hello "world"}}) => ["world"]
   (sort (select-by [:key "*"] {:current {:hello "world", :foo "bar"}})) => ["bar", "world"]
   (sort (select-by [:key "*"] {:current [{:hello "world"}, {:foo "bar"}]})) => ["bar", "world"])
 
 (fact
-  (walk-path [[:root]] {:root ...root..., :current  ...obj...}) => ...root...
-  (walk-path [[:root] [:child] [:key "foo"]] {:root {:foo "bar"}}) => "bar"
+  (walk-path-ng [[:root]] {:root ...root..., :current  ...obj...}) => [...root... []]
+  (walk-path-ng [[:root] [:child] [:key "foo"]] {:root {:foo "bar"}}) => ["bar" [:foo]]
   (walk-path [[:all-children]] {:current {:foo "bar" :baz {:qux "zoo"}}}) => [{:foo "bar" :baz {:qux "zoo"}},
                                                                    {:qux "zoo"}])
 
 (fact
-  (walk-selector [:index "1"] {:current ["foo", "bar", "baz"]}) => "bar"
-  (walk-selector [:index "*"] {:current [:a :b]}) => [:a :b]
-  (walk-selector [:filter [:eq [:path [[:current] [:child] [:key "bar"]]] [:val "baz"]]]
-                 {:current  [{:bar "wrong"} {:bar "baz"}]}) => [{:bar "baz"}])
+  (walk-selector-ng [:index "1"] {:current ["foo", "bar", "baz"]}) => ["bar" [1]]
+  (walk-selector-ng [:index "*"] {:current [:a :b]}) => [[:a :b] [0 1]]
+  (walk-selector-ng [:filter [:eq [:path [[:current] [:child] [:key "bar"]]] [:val "baz"]]]
+                 {:current  [{:bar "wrong"} {:bar "baz"}]}) => [[{:bar "baz"}] [1]])
 
 (fact "selecting places constraints on the shape of the object being selected from"
-  (walk-selector [:index "1"] {:current {:foo "bar"}}) => (throws Exception)
-  (walk-selector [:index "*"] {:current {:foo "bar"}}) => (throws Exception))
+  (walk-selector-ng [:index "1"] {:current {:foo "bar"}}) => (throws Exception)
+  (walk-selector-ng [:index "*"] {:current {:foo "bar"}}) => (throws Exception))
 
 (facts
-  (walk [:path [[:root]]] {:root ...json...}) => ...json...
-  (walk [:path [[:child]]] {:current ...json...}) => ...json...
-  (walk [:path [[:current]]] {:current ...json...}) => ...json...
-  (walk [:path [[:key "foo"]]] {:current {:foo "bar"}}) => "bar"
+  (walk-ng [:path [[:root]]] {:root ...json...}) => [...json... []]
+  (walk-ng [:path [[:child]]] {:current ...json...}) => [...json... []]
+  (walk-ng [:path [[:current]]] {:current ...json...}) => [...json... []]
+  (walk-ng [:path [[:key "foo"]]] {:current {:foo "bar"}}) => ["bar" [:foo]]
   (walk [:path [[:all-children]]]
         {:current
          {:hello {:world "foo"},
@@ -65,22 +65,22 @@
                                             {:world "bar"}]
   (walk [:path [[:all-children]]]
         {:current "scalar"}) => ["scalar"]
-  (walk [:selector [:index "1"]] {:current ["foo", "bar", "baz"]}) => "bar"
-  (walk [:selector [:index "*"]] {:current [:a :b]}) => [:a :b]
-  (walk [:selector [:filter [:eq
+  (walk-ng [:selector [:index "1"]] {:current ["foo", "bar", "baz"]}) => ["bar" [1]]
+  (walk-ng [:selector [:index "*"]] {:current [:a :b]}) => [[:a :b] [0 1]]
+  (walk-ng [:selector [:filter [:eq
                              [:path [[:current]
                                      [:child]
                                      [:key "bar"]]]
                              [:val "baz"]]]]
-        {:current [{:bar "wrong"} {:bar "baz"}]}) => [{:bar "baz"}]
-  (walk [:path [[:root] [:child] [:key "foo"]]
-         [:selector [:filter [:eq [:path [[:current]
-                                          [:child]
-                                          [:key "bar"]]]
-                              [:val "baz"]]]
-          [:path [[:child] [:key "hello"]]]]]
-        {:root {:foo [{:bar "wrong" :hello "goodbye"}
-                      {:bar "baz" :hello "world"}]}}) => ["world"])
+           {:current [{:bar "wrong"} {:bar "baz"}]}) => [[{:bar "baz"}] [1]]
+  (walk-ng [:path [[:root] [:child] [:key "foo"]]
+            [:selector [:filter [:eq [:path [[:current]
+                                             [:child]
+                                             [:key "bar"]]]
+                                 [:val "baz"]]]
+             [:path [[:child] [:key "hello"]]]]]
+           {:root {:foo [{:bar "wrong" :hello "goodbye"}
+                         {:bar "baz" :hello "world"}]}}) => [["world"] [:foo 1 :hello]])
 
 (facts "walking a nil object should be safe"
   (walk [:path [[:root]]] nil) => nil
